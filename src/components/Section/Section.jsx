5712a3302c58d1a16cfadac7f9d06d4a96553a1c
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2022-12-04 18:52:56 +0200

    Remarks

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,8 +1,9 @@
+import { SectionStyled } from './Section.styled.js';
 export const Section = ({ title, children }) => {
   return (
-    <>
+    <SectionStyled>
       <h2>{title}</h2>
       {children}
-    </>
+    </SectionStyled>
   );
 };
